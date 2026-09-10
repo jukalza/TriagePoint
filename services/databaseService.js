6d3b36feb4db8fd8b4db8152906fc2fb57c150f6
jukalza <judykalzadsb@gmail.com>
2026-09-10 02:42:32 +0100

    Enable SSL for production database

--- services/databaseService.js
+++ services/databaseService.js
@@ -8,7 +8,14 @@ const database = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT)
+    port: Number(process.env.DB_PORT),
+
+    ssl: 
+        process.env.NODE_ENV === "production" 
+        ? {
+            rejectUnauthorized: true
+        }
+        : undefined
 });
 
 export default database;
