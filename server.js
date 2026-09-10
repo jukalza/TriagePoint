89583d901d7c12c397c335936e8083e486519908
jukalza <judykalzadsb@gmail.com>
2026-09-10 02:52:53 +0100

    Serve homepage through express

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import path from "path";
+import { fileURLToPath } from "url";
 import nvdRoutes from "./routes/nvdRoutes.js";
 import database from "./services/databaseService.js";
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 // test database connection
 try {
@@ -26,6 +29,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "public",
+            "index.html"
+        )
+    );
+});
 
 // NVD routes
 app.use("/api/nvd", nvdRoutes);
